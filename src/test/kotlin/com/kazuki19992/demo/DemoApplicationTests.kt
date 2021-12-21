4c7a803d1586da88377a108bd47e9cb8f6ef2fed
kazuki19992 <kushida98@gmail.com>
2021-12-21 18:44:49 +0900

    spring boot init

---
+++ src/test/kotlin/com/kazuki19992/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kazuki19992.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
